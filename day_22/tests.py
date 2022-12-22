bcf57ffbbe9462beb9bc699aa43a66f477586f24
Roderick Karlemstrand <roderick@karlemstrand.com>
2022-12-22 16:09:35 +0100

    Implement test, failing

---
+++ day_22/tests.py
@@ -0,0 +1,30 @@
+import unittest
+
+from day_22.Me import Me
+from day_22.a import draw_map, compute_score, my_map
+
+
+class TestDay22(unittest.TestCase):
+    def test_part_a_sample(self):
+        data = """        ...#
+        .#..
+        #...
+        ....
+...#.......#
+........#...
+..#....#....
+..........#.
+        ...#....
+        .....#..
+        .#......
+        ......#.
+
+10R5L5R10L4R5L5"""
+
+        instruction, max_x, max_y = draw_map(data)
+        me = Me(my_map, max_x, max_y)
+        me.move_by_instructions(instruction)
+
+        ans = compute_score(me.position, me.facing)
+
+        self.assertEqual(ans, 6032)
