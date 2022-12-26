3a95e89d62835f1919f0c3dcc407202dd0296bef
Roderick Karlemstrand <roderick@karlemstrand.com>
2022-12-26 19:09:24 +0100

    Implement func to find index and add test

---
+++ day_6/a.py
@@ -0,0 +1,28 @@
+from aocd.models import Puzzle
+
+from helpers import get_day_and_part, submit_answer
+
+
+def find_start_of_packet_marker_index(data):
+    for i in range(len(data) - 3):
+        sub_str = data[i:i+4]
+
+        if len(set(sub_str)) == 4:
+            return i + 4
+
+    return 0
+
+
+def main():
+    day, part = get_day_and_part(__file__)
+
+    puzzle = Puzzle(year=2022, day=day)
+    data = puzzle.input_data
+
+    ans = find_start_of_packet_marker_index(data)
+
+    # submit_answer(ans, day, part)
+
+
+if __name__ == '__main__':
+    main()
