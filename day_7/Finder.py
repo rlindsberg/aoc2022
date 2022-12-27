6a70480d133aa67ecd38d870ddc394b0e67bb906
rlindsberg <roderick@karlemstrand.com>
2022-12-27 17:03:25 +0100

    Simplify main function, implement parse cmd

--- day_7/Finder.py
+++ day_7/Finder.py
@@ -33,3 +33,34 @@ class Finder:
         parent = self.dir_dict[parent_path]
 
         return Folder(dir_name, parent)
+
+    def parse_raw_cmd_input(self, cmd_str):
+        cmd_list = cmd_str.split('\n')
+        cmd = cmd_list[0]
+        res = cmd_list[1:]
+
+        if cmd[:2] == 'cd':
+            target_dir = cmd[3:]
+
+            cmd_type = cmd[:2]
+            argument = target_dir
+
+            return cmd_type, argument
+
+        elif cmd[:2] == 'ls':
+            cmd_type = cmd[:2]
+
+            return cmd_type, res
+            # for item in res:
+            #     # item can be file or dir
+            #     if item[:3] == 'dir':
+            #         # this is a dir
+            #         dir_name = item[4:]
+            #
+            #         if dir_name not in dir_dict.keys():
+            #             # index this dir
+            #             temp_dir = Folder(dir_name, )
+
+    def exec_cmd(self, cmd_str):
+        # cd, arg or ls, response
+        cmd_type, arg_or_res = self.parse_raw_cmd_input(cmd_str)
--- day_7/a.py
+++ day_7/a.py
@@ -1,7 +1,18 @@
+from day_7.Finder import Finder
+from day_7.Folder import Folder
 from helpers import get_day_and_part, submit_answer, get_input_data
 
 
-def something(data):
+def sum_all_sub_folder_sizes(data, at_most):
+    input_list = data.split("$ ")[1:]
+
+    # some preparation
+    finder = Finder()
+
+    # parse input and build dir map
+    for cmd_str in input_list:
+
+        finder.exec_cmd(cmd_str)
 
     return 0
 
@@ -12,10 +23,10 @@ def main():
     data = get_input_data(day)
 
     # solution
-    ans = something(data)
+    ans = sum_all_sub_folder_sizes(data)
 
     # submit
-    submit_answer(ans, day, part)
+    # submit_answer(ans, day, part)
 
 
 if __name__ == '__main__':
