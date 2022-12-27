332988b6e67b9c4a7bb19c72438ef3d274dd22be
rlindsberg <roderick@karlemstrand.com>
2022-12-27 19:22:42 +0100

    Add test for when at / do cd .. will crash program

--- day_7/tests.py
+++ day_7/tests.py
@@ -26,11 +26,42 @@ class TestDay7(unittest.TestCase):
         self.assertEqual(res.parent.name, '/')
         self.assertEqual(res.name, 'a')
 
-
     def test_part_a(self):
-
         data = """
 $ cd /
+$ ls
+dir a
+dir b
+3 file1-root
+$ cd a
+$ ls
+5 file2-a
+$ cd ..
+$ cd b
+$ ls
+7 file3-b"""
+
+        """
+        /
+            - a
+                - 5kb file2-a
+
+            - b
+                - 7kb file3-b
+
+            - 3 kb file1-root
+        """
+        print(data)
+
+        ans = sum_all_sub_folder_sizes(data, at_most=100000)
+
+        self.assertEqual(ans, 12)
+
+    def test_part_a_2(self):
+        """go back too many times, shouldn't crash"""
+        data = """
+$ cd /
+$ cd ..
 $ ls
 dir a
 dir b
