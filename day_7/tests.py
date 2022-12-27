e838d2015449d60d14b05728059b9f6344aff67d
rlindsberg <roderick@karlemstrand.com>
2022-12-27 16:46:51 +0100

    Add test for parse a file string

--- day_7/tests.py
+++ day_7/tests.py
@@ -1,9 +1,21 @@
 import unittest
 
+from day_7.Finder import Finder
 from day_7.a import sum_all_sub_folder_sizes
 
 
 class TestDay7(unittest.TestCase):
+    def test_parse_file(self):
+        file_str = '5 file2-a'
+
+        finder = Finder()
+
+        res = finder.parse_file('/', file_str)
+
+        self.assertEqual(res.parent.name, '/')
+        self.assertEqual(res.size, 5)
+        self.assertEqual(res.name, 'file2-a')
+
     def test_part_a(self):
 
         data = """
