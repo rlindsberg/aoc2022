ea7e234ac8d92eb75abb4569099dc4848ab8203d
Roderick Karlemstrand <roderick@karlemstrand.com>
2022-12-28 20:36:33 +0100

    Day 10 done! This task is tricky (#9)
    * Add test for addx func
    
    * Implement CPU
    
    * Implement noop and addx, test passed
    
    * Test passed:
    During the 20th cycle, register X has the value 21, so the signal 
    strength is 20 * 21 = 420. (The 20th cycle occurs in the middle of the 
    second addx -1, so the value of register X is the starting value, 1, 
    plus all of the other addx values up to that point: 1 + 15 - 11 + 6 - 3 
    + 5 - 1 - 8 + 13 + 4 = 21.)
    During the 60th cycle, register X has the value 19, so the signal 
    strength is 60 * 19 = 1140.
    During the 100th cycle, register X has the value 18, so the signal 
    strength is 100 * 18 = 1800.
    During the 140th cycle, register X has the value 21, so the signal 
    strength is 140 * 21 = 2940.
    During the 180th cycle, register X has the value 16, so the signal 
    strength is 180 * 16 = 2880.
    During the 220th cycle, register X has the value 18, so the signal 
    strength is 220 * 18 = 3960.
    
    * Implement sum of signal strengths, passing
    
    * Part 1 done
    
    * Start part 2, disable print
    
    * Finish part 2. Use space to be able to see the letters

---
+++ day_10/CPU.py
@@ -0,0 +1,57 @@
+class CPU:
+    def __init__(self):
+        # only one register called X
+        self.register = 1
+        # starts with the first cycle
+        self.counter = 0
+        # addx takes two cycles
+        self.addx_timer = 1
+
+        self.sum_signal_strength = 0
+
+    def parse_and_exec_cmd(self, raw_input: str):
+        # the start of a new cycle
+        self.counter += 1
+        # print(f'Start of cycle {self.counter}')
+
+        # now we are in the middle of a cycle
+        if self.counter in [20, 60, 100, 140, 180, 220]:
+            # print('time to measure signal strength!')
+            # print(self.register)
+            self.sum_signal_strength += self.counter * self.register
+
+        # CRT draw
+        if self.counter % 41 == 0:
+            print('')
+            self.counter = 1
+        # self.counter - 1 because CRT draws at position 0 at cycle 1
+        if self.register - 1 <= self.counter - 1 <= self.register + 1:
+            # draw lit pixel
+            print('#', end='')
+        else:
+            print(' ', end='')
+
+        if raw_input == 'noop':
+            # noop has no effect
+            pass
+        elif raw_input == '':
+            # end of cmd
+            return
+        else:
+            # addx
+            cmd_list = raw_input.split(' ')
+            op = cmd_list[0]
+            arg = cmd_list[1]
+
+            if op == 'addx':
+                if self.addx_timer == 0:
+                    # reset timer
+                    self.addx_timer = 1
+
+                    # at the end of a cycle
+                    self.register += int(arg)
+                else:
+                    self.addx_timer -= 1
+                    self.parse_and_exec_cmd(raw_input)
+
+        # print(f'End of cycle {self.counter}')
