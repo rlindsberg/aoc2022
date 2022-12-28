8d156074bab31a0f10f9540bdf111a8adc574ab5
Roderick Karlemstrand <roderick@karlemstrand.com>
2022-12-28 11:04:25 +0100

    Add tests for part b, failing

--- day_8/tests.py
+++ day_8/tests.py
@@ -3,6 +3,7 @@ import unittest
 from day_7.Finder import Finder
 from day_7.a import sum_all_sub_folder_sizes
 from day_8.a import count_visible_trees
+from day_8.b import compute_scenic_score
 
 
 class TestDay7(unittest.TestCase):
@@ -19,3 +20,16 @@ class TestDay7(unittest.TestCase):
         ans = count_visible_trees(data)
 
         self.assertEqual(ans, 21)
+
+    def test_part_b(self):
+        data = """30373
+25512
+65332
+33549
+35390"""
+
+        print(data)
+
+        ans = compute_scenic_score(data)
+
+        self.assertEqual(ans, 8)
