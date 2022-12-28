fdfabaaa571aa301cadf850da8730cc457c7cf48
Roderick Karlemstrand <roderick@karlemstrand.com>
2022-12-28 10:38:02 +0100

    Implement func, but ans is too high

---
+++ day_8/a.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+from helpers import get_day_and_part, submit_answer, get_input_data
+
+
+def count_visible_trees(data):
+    tree_2d_list = [[int(tree) for tree in row] for row in data.split("\n")]
+    tree_array = np.array(tree_2d_list)
+
+    n_visible_tree = 4 * (len(tree_array) - 2) + 4
+    # second row - second last row
+    for y in range(1, len(tree_array) - 1):
+        # second column - second last column
+        for x in range(1, len(tree_array) - 1):
+            tree = tree_array[y][x]
+
+            north = tree_array[y - 1][x]
+            south = tree_array[y + 1][x]
+            east = tree_array[y][x + 1]
+            west = tree_array[y][x - 1]
+
+            if tree <= min(north, east, south, west):
+                print('potential tree')
+                pass
+            else:
+                # visible tree
+                n_visible_tree += 1
+
+    return n_visible_tree
+
+
+def main():
+    # get input data
+    day, part = get_day_and_part(__file__)
+    data = get_input_data(day)
+
+    # solution
+    ans = count_visible_trees(data)
+
+    # submit
+    # submit_answer(ans, day, part)
+
+
+if __name__ == '__main__':
+    main()
